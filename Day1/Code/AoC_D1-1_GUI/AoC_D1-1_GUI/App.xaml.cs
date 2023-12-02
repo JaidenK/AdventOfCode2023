38ddd604de3d846255aa41c4a493884475340ef7
JaidenK <king.jaiden@yahoo.com>
2023-12-01 20:36:16 -0800

    Create empty parser in prep for part 2

--- Day1/Code/AoC_D1-1_GUI/AoC_D1-1_GUI/App.xaml.cs
+++ Day1/Code/AoC_D1-1_GUI/AoC_D1-1_GUI/App.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using AoC_D1_1_GUI.Model;
+using AoC_D1_1_GUI.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +17,42 @@ namespace AoC_D1_1_GUI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if(e.Args.Length > 0)
+            {
+                Run_Headless(e);
+            }
+            else
+            {
+                var Wnd = new MainWindow();
+                Wnd.Show();
+            }
+        }
+
+        private void Run_Headless(StartupEventArgs e)
+        {
+            // Console.WriteLine won't show up in the actual command window
+            // and I don't know why. Not worth trying to fix right now 
+            // for this little AoC challenge so I'll just write the output
+            // to a file.
+
+            try
+            {
+                string input_filename = e.Args[0];
+                string output_filename = e.Args.Length > 1 ? e.Args[1] : "out.txt";
+
+                var lines = File.ReadAllLines(input_filename);
+                var result = new Parser().ParseLines(lines);
+
+                File.WriteAllText(output_filename, result.FullCalValue.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            Application.Current.Shutdown();
+        }
     }
 }
--- Day1/Code/AoC_D1-1_GUI/AoC_D1-1_Tests/ParserTests1.cs
+++ Day1/Code/AoC_D1-1_GUI/AoC_D1-1_Tests/ParserTests1.cs
@@ -8,68 +8,42 @@ namespace AoC_D1_1_Tests
     [TestClass]
     public class ParserTests1
     {
-        [TestMethod]
-        public void Test1_Simple()
+        public static void BasicParserTest(IParser parser, string input, int expectedResult)
         {
-            // Arrange 
-            string input = "1abc2";
-            int expectedResult = 12;
-            IParser parser = new Parser();
-
             // Act
             IParserResult result = parser.ParseLine(input);
             int calVal = result.CalValue;
 
             // Verify
             Assert.AreEqual(expectedResult, calVal);
+        }
+
+        [TestMethod]
+        public void Test1_Simple()
+        {
+            IParser parser = new Parser();
+            BasicParserTest(parser, "1abc2", 12);
         }
 
         [TestMethod]
         public void Test2_2digitsInString()
         {
-            // Arrange 
-            string input = "pqr3stu8vwx";
-            int expectedResult = 38;
             IParser parser = new Parser();
-
-            // Act
-            IParserResult result = parser.ParseLine(input);
-            int calVal = result.CalValue;
-
-            // Verify
-            Assert.AreEqual(expectedResult, calVal);
+            BasicParserTest(parser, "pqr3stu8vwx", 38);
         }
 
         [TestMethod]
         public void Test3_manyDigitsInString()
         {
-            // Arrange 
-            string input = "a1b2c3d4e5f";
-            int expectedResult = 15;
             IParser parser = new Parser();
-
-            // Act
-            IParserResult result = parser.ParseLine(input);
-            int calVal = result.CalValue;
-
-            // Verify
-            Assert.AreEqual(expectedResult, calVal);
+            BasicParserTest(parser, "a1b2c3d4e5f", 15);
         }
 
         [TestMethod]
         public void Test4_oneDigitInString()
         {
-            // Arrange 
-            string input = "treb7uchet";
-            int expectedResult = 77;
             IParser parser = new Parser();
-
-            // Act
-            IParserResult result = parser.ParseLine(input);
-            int calVal = result.CalValue;
-
-            // Verify
-            Assert.AreEqual(expectedResult, calVal);
+            BasicParserTest(parser, "treb7uchet", 77);
         }
 
         [TestMethod]
@@ -92,6 +66,25 @@ namespace AoC_D1_1_Tests
             // Verify
             Assert.AreEqual(expectedResult, result.FullCalValue);
 
+        }
+
+        [TestMethod]
+        public void AlphaTest1()
+        {
+            // These are all the same as the original tests
+            IParser parser = new AlphaNumericParser();
+            BasicParserTest(parser, "1abc2", 12);
+            BasicParserTest(parser, "pqr3stu8vwx", 38);
+            BasicParserTest(parser, "a1b2c3d4e5f", 15);
+            BasicParserTest(parser, "treb7uchet", 77);
+            // These are new
+            BasicParserTest(parser,"two1nine",29);
+            BasicParserTest(parser,"eightwothree",83);
+            BasicParserTest(parser,"abcone2threexyz",13);
+            BasicParserTest(parser,"xtwone3four",24);
+            BasicParserTest(parser,"4nineeightseven2",42);
+            BasicParserTest(parser,"zoneight234",14);
+            BasicParserTest(parser,"7pqrstsixteen",76);
         }
     }
 }
