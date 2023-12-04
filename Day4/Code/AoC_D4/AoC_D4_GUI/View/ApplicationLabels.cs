6d1bac044f61f6930f84682629d25863e37cfeb6
JaidenK <king.jaiden@yahoo.com>
2023-12-04 14:22:24 -0800

    Day 4 - GUI implementation of part 1 with example input

---
+++ Day4/Code/AoC_D4/AoC_D4_GUI/View/ApplicationLabels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_D4_GUI.View
+{
+    internal class ApplicationLabels
+    {
+        public static string NoFilenameDefaultText = "Enter filename here...";
+    }
+}
---
+++ Day4/Code/AoC_D4/AoC_D4_GUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using AoC_D4;
+using AoC_D4_GUI.Core;
+using AoC_D4_GUI.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_D4_GUI.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        #region Interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+        #endregion
+
+        private string filename = "";
+        public string Filename { get => filename; set => SetProperty(ref filename, value); }
+
+        private List<ICard> cards = new List<ICard>();
+        public List<ICard> Cards { get => cards; set => SetProperty(ref cards, value); }
+
+        public ulong PointSum => CardUtil.SumPointValues(Cards);
+
+        public MainViewModel()
+        {
+            PropertyChanged += MainViewModel_PropertyChanged;
+
+            cards = new CardFactory().GetCards(ExampleInput.all_input);
+        }
+
+        private void MainViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch(e.PropertyName)
+            {
+                case "Cards":
+                    PropertyChanged?.Invoke(sender, new PropertyChangedEventArgs("PointSum"));
+                    break;
+            }
+        }
+
+    }
+}
