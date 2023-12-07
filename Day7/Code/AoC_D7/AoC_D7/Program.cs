aecdc1e9a2d6ded81a7ec9fb99a0b836f3a1d9ad
JaidenK <king.jaiden@yahoo.com>
2023-12-07 01:24:12 -0800

    Day 7 - Added some benchmarking output

--- Day7/Code/AoC_D7/AoC_D7/Program.cs
+++ Day7/Code/AoC_D7/AoC_D7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,16 +11,29 @@ namespace AoC_D7
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            Stopwatch sw = Stopwatch.StartNew();
+
             var input = File.ReadAllLines("input.txt");
-            
+            var t1 = sw.Elapsed;
+
             var set = SetFactory.ParseLines(input);
+            var t2 = sw.Elapsed;
             var winnings = set.ComputeWinnings();
+            var t3 = sw.Elapsed;
             Console.WriteLine($"Winnings (Part 1): {winnings}");
-            
+
             set = SetFactory.ParseLines(input, usingJokers: true);
+            var t4 = sw.Elapsed;
             winnings = set.ComputeWinnings();
             Console.WriteLine($"Winnings (Part 2): {winnings}");
+            var t5 = sw.Elapsed;
+
+            Console.WriteLine($"Opening file:\t\t{t1}");
+            Console.WriteLine($"Part 1 parsing:\t\t{t2-t1}");
+            Console.WriteLine($"Part 1 computing:\t{t3-t2}");
+            Console.WriteLine($"Part 2 parsing:\t\t{t4-t3}");
+            Console.WriteLine($"Part 2 computing:\t{t5-t4}");
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
