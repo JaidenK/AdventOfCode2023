3ccba76d5dc64bb9f6d799c01552a4896c46c0de
JaidenK <king.jaiden@yahoo.com>
2023-12-06 22:40:05 -0800

    Day 7 - Card value tests written

--- Day7/Code/AoC_D7/AoC_D7_Tests/HandTests.cs
+++ Day7/Code/AoC_D7/AoC_D7_Tests/HandTests.cs
@@ -9,6 +9,24 @@ namespace AoC_D7_Tests
     public class HandTests
     {
         [TestMethod]
+        public void CardValueTests()
+        {
+            Assert.AreEqual(2, new Two().Strength);
+            Assert.AreEqual(3, new Three().Strength);
+            Assert.AreEqual(4, new Four().Strength);
+            Assert.AreEqual(5, new Five().Strength);
+            Assert.AreEqual(6, new Six().Strength);
+            Assert.AreEqual(7, new Seven().Strength);
+            Assert.AreEqual(8, new Eight().Strength);
+            Assert.AreEqual(9, new Nine().Strength);
+            Assert.AreEqual(10, new Ten().Strength);
+            Assert.AreEqual(11, new Jack().Strength);
+            Assert.AreEqual(12, new Queen().Strength);
+            Assert.AreEqual(13, new King().Strength);
+            Assert.AreEqual(14, new Ace().Strength);
+        }
+
+        [TestMethod]
         public void HandFactoryTest()
         {
             var hand1 = new HandFactory().BuildHand("A2345 123");
