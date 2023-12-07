81c21e50b53769a905e188ee494070f0eadc0047
JaidenK <king.jaiden@yahoo.com>
2023-12-06 22:38:23 -0800

    Day 7 - Hand factory works

--- Day7/Code/AoC_D7/AoC_D7/CardFactory.cs
+++ Day7/Code/AoC_D7/AoC_D7/CardFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AoC_D7.Cards;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,23 @@ namespace AoC_D7
     {
         internal ICard BuildCard(char letter)
         {
-            throw new NotImplementedException();
+            switch(char.ToUpper(letter))
+            {
+                case 'A': return new Ace();
+                case '2': return new Two();
+                case '3': return new Three();
+                case '4': return new Four();
+                case '5': return new Five();
+                case '6': return new Six();
+                case '7': return new Seven();
+                case '8': return new Eight();
+                case '9': return new Nine();
+                case 'T': return new Ten();
+                case 'J': return new Jack();
+                case 'Q': return new Queen();
+                case 'K': return new King();
+            }
+            throw new Exception($"Invalid card: {letter}");
         }
     }
 }
