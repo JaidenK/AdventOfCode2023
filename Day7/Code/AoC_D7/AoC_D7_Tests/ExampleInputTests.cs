842c376d40489fde0c823d7d5270ca245c75cc76
JaidenK <king.jaiden@yahoo.com>
2023-12-06 21:37:04 -0800

    Day 7 - Example E2E test written

--- Day7/Code/AoC_D7/AoC_D7_Tests/ExampleInputTests.cs
+++ Day7/Code/AoC_D7/AoC_D7_Tests/ExampleInputTests.cs
@@ -6,9 +6,27 @@ namespace AoC_D7_Tests
     [TestClass]
     public class ExampleInputTests
     {
+        readonly string[] example_input =
+        {
+            "32T3K 765",
+            "T55J5 684",
+            "KK677 28",
+            "KTJJT 220",
+            "QQQJA 483"
+        };
+
         [TestMethod]
         public void ExampleInput_E2E()
         {
+            // Parse the input
+            ISet set = SetFactory.ParseLines(example_input);
+            // Sort by rank
+            set.SortHands();
+            // Calculate the winnings
+            var winnings = set.ComputeWinnings();
+            // Verify the output
+            Assert.AreEqual(6440, winnings);
+
         }
     }
 }
