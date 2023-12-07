987399b8eb6f7befd3705121ec6f6a5a237e962d
JaidenK <king.jaiden@yahoo.com>
2023-12-06 21:32:04 -0800

    Day 7 - Initial commit

---
+++ Day7/Code/AoC_D7/AoC_D7_Tests/ExampleInputTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AoC_D7_Tests
+{
+    [TestClass]
+    public class ExampleInputTests
+    {
+        [TestMethod]
+        public void ExampleInput_E2E()
+        {
+        }
+    }
+}
---
+++ Day7/Code/AoC_D7/AoC_D7_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AoC_D7_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AoC_D7_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e689d3df-463b-4199-894c-a03887163a68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
